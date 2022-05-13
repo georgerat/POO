40be2fbffa1e6211de81bc09adce036d3ff30fd1
georgerat <ratgeorgeraul2018@gmail.com>
2022-05-13 08:48:38 +0300

    add P12

---
+++ P12/P12/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace P12
+{
+    //Sa se implementeze clasa tablou cu membrii data unidimensionali cu valori integi precum si dimensiunea acestuia. Sa se
+    //scrie un constructor cu un parametru reprezentand lungimea tabloului si un indexator unidimensional in care sa se
+    //trateze eventuale erori posibile.
+
+    class Tablou
+    {
+        int[] a;
+        public int dimensiune;
+        public bool err;
+
+        public Tablou(int lungime)
+        {
+            a = new int[lungime];
+            dimensiune = lungime;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < dimensiune)
+                {
+                    err = false;
+                    return a[index];
+                }
+                else
+                {
+                    err = true;
+                    return -1;
+                }
+            }
+            set
+            {
+                if (index >= 0 && index < dimensiune)
+                {
+                    a[index] = value;
+                    err = false;
+                }
+                else
+                    err = true;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tablou t = new Tablou(5);
+            for (int i = 0; i < t.dimensiune * 2; i++)
+                t[i] = 10 * i;
+            for (int i = 0; i < t.dimensiune * 2; i++)
+            {
+                int x = t[i];
+                if (t.err)
+                    Console.Write("\n t[{0}] depaseste marginile", i);
+                else
+                    Console.Write("{0}\t", x);
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
