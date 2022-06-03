d8c539325d8e2daedfbcb08630c6c92c040d20e9
georgerat <ratgeorgeraul2018@gmail.com>
2022-06-03 10:40:46 +0300

    add P21

---
+++ P21/P21/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace P21
+{
+    //Rutina de tratare a unui eveniment.
+
+    class Program
+    {
+        static void HandlerEv()
+        {
+            Console.WriteLine("Evenimentul s-a produs.");
+        }
+
+        static void Main()
+        {
+            MyEvent ev = new MyEvent();
+            ev.activare += new MyEventHandlerDelegate(HandlerEv);
+            ev.Fire();
+        }
+    }
+
+    public delegate void MyEventHandlerDelegate();
+
+    class MyEvent
+    {
+        public event MyEventHandlerDelegate activare;
+
+        public void Fire()
+        {
+            activare();
+        }
+    }
+}
