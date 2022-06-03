ebd2bd54049d7969c2c15129c63fdf088034d0f2
georgerat <ratgeorgeraul2018@gmail.com>
2022-06-03 08:43:42 +0300

    add P18

---
+++ P18/P18/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P18
+{
+    //Delegare care apeleaza metode statice
+
+    public delegate double MyDelegate(double a);
+
+    class Circle
+    {
+        const float PI = 3.14159f;
+
+        static double Aria(double r)
+        {
+            return PI * r * r;
+        }
+
+        static double LungFrontiera(double r)
+        {
+            return 2 * PI * r;
+        }
+
+        public static void Main()
+        {
+            double raza = 3;
+            MyDelegate del = new MyDelegate(Aria);
+            Console.WriteLine($"Aria : {del(raza):#.##}");
+            del = new MyDelegate(LungFrontiera);
+            Console.WriteLine($"Lungimea frontierei : {del(raza):#.##}");
+        }
+    }
+}
